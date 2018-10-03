5abd11786ef6e86de75851bcfc61c7d9714f4ffe
rys <ryseok@gmail.com>
2018-10-03 21:49:20 +0900

    181003 : 김왼손 ===> [입문] 한입에 쏙 파이썬 10강 까지 들음

---
+++ leftHandCoding/Day01_HelloWord.py
@@ -0,0 +1,17 @@
+#첫 코딩은 헬로우 월드 ;-)
+# print("hell world")
+# print("나의 노트북 좀더 힘내자!!")
+# print("양수>>>",1)
+# print("음수>>>",-1)
+# print("실수>>>",3.14)
+# print(1+1)
+# print(2-5)
+# print(3*10)
+# print(5/10)
+print("==========================")
+#이것도 되는지 TEST
+# num=1
+# num2=2
+# result=num+num2
+# print(result)
+
